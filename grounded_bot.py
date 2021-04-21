c489d693cc7d3562a729713b3dba83a67000f199
Eduardo Barrancos <eduardobarrancps11488@gmail.com>
2021-04-22 00:02:33 +0100

    Switch to environ file

--- grounded_bot.py
+++ grounded_bot.py
@@ -1,15 +1,9 @@
 import keep_alive
 import asyncio
 import discord
-import json
+import os
 from discord.ext import commands
 from database.databaseHandler import DatabaseHandler
-
-
-# import Bot Token
-with open('bot_token.json') as json_file:
-    data = json.load(json_file)
-    TOKEN = data["TOKEN"]
 
 myBot = commands.Bot(('!', '$'))
 
@@ -31,5 +25,7 @@ class Echo(commands.Cog):
 
 
 myBot.add_cog(Echo())
-myBot.run(TOKEN)
+
 keep_alive.keep_alive()
+
+myBot.run(os.environ.get('TOKEN'))
