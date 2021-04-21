ca4969f02980a17b740e6109e298229a5f99abe9
Eduardo Barrancos <eduardobarrancps11488@gmail.com>
2021-04-21 12:35:38 +0100

    Bot Creation | Echo Command

--- grounded_bot.py
+++ grounded_bot.py
@@ -2,6 +2,7 @@ import keep_alive
 import asyncio
 import discord
 import json
+from discord.ext import commands
 
 
 # import Bot Token
@@ -9,4 +10,24 @@ with open('bot_token.json') as json_file:
     data = json.load(json_file)
     TOKEN = data["TOKEN"]
 
-keep_alive.keep_alive()
+myBot = commands.Bot(('!', '$'))
+
+
+@myBot.event
+async def on_ready():
+    print(f'Logged in as {myBot.user.name} (ID: {myBot.user.id})')
+    print('-------')
+
+
+class Echo(commands.Cog):
+    def _init_(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def echo(self, ctx, message: str):
+        await ctx.send(message)
+
+
+myBot.add_cog(Echo())
+myBot.run(TOKEN)
+# keep_alive.keep_alive()
