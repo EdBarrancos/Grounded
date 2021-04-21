e4c9f9835270486b92654dd4b98eace730918a5f
Eduardo Barrancos <eduardobarrancps11488@gmail.com>
2021-04-21 02:07:54 +0100

    Import token

--- grounded_bot.py
+++ grounded_bot.py
@@ -1,6 +1,12 @@
 import keep_alive
 import asyncio
 import discord
+import json
 
+
+# import Bot Token
+with open('bot_token.json') as json_file:
+    data = json.load(json_file)
+    TOKEN = data["TOKEN"]
 
 keep_alive.keep_alive()
