6dd7c1f5d6bc841380474badd920fe435ba77fbb
Eduardo Barrancos <eduardobarrancps11488@gmail.com>
2021-04-24 12:52:35 +0100

    Create Database Server Table

--- src/database/databaseHandler.py
+++ src/database/databaseHandler.py
@@ -4,6 +4,7 @@ import asyncio
 
 # Third Party Imports
 import sqlite3
+import logging
 
 # Local Application Imports
 import exceptions.databaseExceptions as databaseExceptions
@@ -14,21 +15,59 @@ class ConnectionNotEstablished(databaseExceptions.DatabaseException):
     """ Connection Failed to be Established """
     def __init__(self, message="Connection Failed to be Established"):
         super(ConnectionNotEstablished, self).__init__(message)
+
+class TableNotCreated(databaseExceptions.DatabaseException):
+    """ Failed to Create a Table in the Database """
+    def __init__(self, message="Table Failed to be Created"):
+        spuer(TableNotCreated, self).__init__(message)
         
 
 class DatabaseHandler():
     def __init__(self, owner, databaseFile="./databases/database.db"):
         self.owner = owner
+        self.sqlCreateServerTable = """CREATE TABLE IF NOT EXISTS server (
+                                            id integer PRIMARY KEY,
+                                            name text NOT NULL,
+                                            owner_id integer NOT NULL,
+                                            text_channel_id integer,
+                                            voice_channel_id integer                                            
+                                        );  """
+
+
         self.connection = self.CreateConnection(databaseFile)
 
+        #Error Checking
+        
+        if self.connection is None: raise ConnectionNotEstablished("Failed to establish database connection")
+        self.CreateTable(self.sqlCreateServerTable)
+
     def CreateConnection(self, dbFile):
+        logging.info("Database Connection Initializing ...")
+
         conn = None
         try:
             conn = sqlite3.connect(dbFile)
         except sqlite3.Error as e:
             raise ConnectionNotEstablished(e.__str__())
 
+        logging.info("Database Connection Initialized Successfully")
+
         return conn
+
+    def CreateTable(self, createTableStatement):
+        logging.info("Database Table Initializing...")
+
+        try:
+            c = self.connection.cursor()
+            c.execute(createTableStatement)
+        except sqlite3.Error as e:
+            raise TableNotCreated(e.__str__())
+
+        logging.info("Database Table Initialized Successfully")
+
+        return
+    
+    
 
     async def AddServer(self, server):
         pass
