91fb48ebb2233e985549b7655fde1b2aefa6c13a
Eduardo Barrancos <eduardobarrancps11488@gmail.com>
2021-04-24 02:33:22 +0100

    Code Decoupling

---
+++ src/main.py
@@ -0,0 +1,25 @@
+# Standard Library Imports
+import asyncio
+import logging
+import os
+
+# Third Party Imports
+import discord
+from decouple import config
+
+# Local Application Imports
+import keep_alive
+from grounded_bot import MyBot
+
+
+logging.basicConfig(format='%(levelname)s:%(message)s', 
+                    filename="bot_logs/grounded_bot.log", 
+                    level=logging.INFO, filemode="w")
+                    
+myBot = MyBot(("!", "$"))
+
+# keep_alive.keep_alive()
+
+# myBot.run(os.getenv('TOKEN'))
+
+myBot.run(config("TOKEN"))
